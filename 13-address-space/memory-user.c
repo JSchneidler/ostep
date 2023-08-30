b262d4191d64d4dfd2c32e3d6fe76a7c2287a2f5
Jordan Schneidler <jordan.schneidler@gmail.com>
2023-08-29 21:46:21 -0600

    Chapter 13 solution

---
+++ 13-address-space/memory-user.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    int mem_mb = atoi(argv[1]);
+    printf("%d: %dMb\n", getpid(), mem_mb);
+
+    int size = mem_mb * 1024 * 1024;
+    int* array = malloc(size);
+
+    for(int i = 0; i < size / sizeof(int); i++)
+        array[i] = -1;
+
+    for(;;){}
+
+    return 0;
+}
